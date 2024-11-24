951e843047c8f82da1d6945a6aeaa996da328c7d
Luc Ferrara <lucjferrara@gmail.com>
2024-11-24 03:04:11 -0500

    Made a file to test running a pacman policy on the custom environment. Doesn't work yet since the ghosts haven't been implemented. Also need to finish gym functions such as step

--- Custom_Environment/test_pacman_env.py
+++ Custom_Environment/test_pacman_env.py
@@ -1,7 +1,31 @@
+import sys
+import os 
+
+sys.path.append(os.path.abspath("../Pacman_Training"))
+
+import torch
+
+from Pacman_reload_dqn import load_execution
+from pacman_neural_network import DQN
 from custom_pacman_env import PacmanEnv 
+
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
 
 # Create the Pacman environment
 env = PacmanEnv()
+
+n_actions = env.pacman_action_space.n
+policy_net = DQN(n_actions).to(device)
+
+load_execution(policy_net, only_policy_net=True, dirpath="")
+
+def select_action(state):
+    return policy_net(state).max(1).indices.view(1, 1)
+
 
 # # Test environment
 # env.render()
@@ -27,7 +51,14 @@ env = PacmanEnv()
 # for i in range(20):
 #     env.move_pacman(4)
 
-
+state = env.grid 
+env.render()
 #Test random movements
 for i in range(100):
-    env.move_pacman(env.pacman_action_space.sample())
+    state = torch.tensor(state, device=device, dtype=torch.float32).unsqueeze(0) / 255.0
+    state = state.unsqueeze(1)
+    action = select_action(state)
+    print(f"Action: {action.item()}")
+    env.move_pacman(action.item())
+    env.render()
+    state = env.grid 
