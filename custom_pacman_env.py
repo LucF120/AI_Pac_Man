a36d568df1964279557deb3f07055ad49ffd3379
Luc Ferrara <lucjferrara@gmail.com>
2024-11-24 02:08:04 -0500

    Simplified pacman movement functions

--- custom_pacman_env.py
+++ custom_pacman_env.py
@@ -36,7 +36,7 @@ class PacmanEnv(gym.Env):
         """Render the environment."""
         plt.imshow(self.grid, cmap="gray", interpolation="nearest")
         plt.draw()
-        plt.pause(0.001)  # Pause for 0.1 seconds to show the update
+        plt.pause(0.00001)  # Pause for 0.1 seconds to show the update
         plt.clf()  # Clear the figure for the next frame
 
     def _is_free(self, pos):
@@ -44,6 +44,8 @@ class PacmanEnv(gym.Env):
     def _check_game_over(self):
         """Check if Pacman is caught by a ghost."""
 
+    def get_pacman_coordinates(self):
+        return np.column_stack(np.where(self.grid == 223))
     def move_pacman(self, action):
         # 0 = NOOP
         if action == 0:
@@ -65,63 +67,55 @@ class PacmanEnv(gym.Env):
         
     def move_pacman_left(self):
         updated_grid = np.copy(self.grid)
-        pacman_coordinates = np.column_stack(np.where(self.grid == 223))
-        legal_move = True
+        pacman_coordinates = self.get_pacman_coordinates()
         for coordinate in sorted(pacman_coordinates, key=lambda x: x[1]):
             if updated_grid[coordinate[0], coordinate[1] - 1] == 192:
                 if not self.is_valid_pip_location(coordinate[1], coordinate[0]):
-                    legal_move = False
-                    break
+                    return
             updated_grid[coordinate[0], coordinate[1]] = 64
             updated_grid[coordinate[0], coordinate[1]-1] = 223
-        if legal_move:
-            self.grid = updated_grid
+        # Update the grid after pacman moved left 
+        self.grid = updated_grid
 
     def move_pacman_right(self):
         updated_grid = np.copy(self.grid)
-        pacman_coordinates = np.column_stack(np.where(self.grid == 223))
-        legal_move = True
+        pacman_coordinates = self.get_pacman_coordinates()
         for coordinate in sorted(pacman_coordinates, key=lambda x: x[1], reverse=True):
             if updated_grid[coordinate[0], coordinate[1] + 1] == 192:
                 if not self.is_valid_pip_location(coordinate[1], coordinate[0]):
-                    legal_move = False
-                    break
+                    return
             updated_grid[coordinate[0], coordinate[1]] = 64
             updated_grid[coordinate[0], coordinate[1]+1] = 223
         
-        if legal_move:
-            self.grid = updated_grid
+        # Update the grid after pacman moved right 
+        self.grid = updated_grid
 
 
     def move_pacman_up(self):
         updated_grid = np.copy(self.grid)
-        pacman_coordinates = np.column_stack(np.where(self.grid == 223))
-        legal_move = True
-        pacman_coordinates = np.column_stack(np.where(self.grid == 223))
+        pacman_coordinates = self.get_pacman_coordinates()
         for coordinate in sorted(pacman_coordinates, key=lambda x: x[0]):
             if updated_grid[coordinate[0]-1, coordinate[1]] == 192:
                 if not self.is_valid_pip_location(coordinate[1], coordinate[0]):
-                    legal_move = False
                     return
             updated_grid[coordinate[0], coordinate[1]] = 64
             updated_grid[coordinate[0]-1, coordinate[1]] = 223
 
-        if legal_move:
-            self.grid = updated_grid
+        # Update the grid after pacman moved up 
+        self.grid = updated_grid
     
     def move_pacman_down(self):
         updated_grid = np.copy(self.grid)
-        pacman_coordinates = np.column_stack(np.where(self.grid == 223))
-        legal_move = True
+        pacman_coordinates = self.get_pacman_coordinates()
         for coordinate in sorted(pacman_coordinates, key=lambda x: x[0], reverse=True):
             if updated_grid[coordinate[0]+1, coordinate[1]] == 192:
                 if not self.is_valid_pip_location(coordinate[1], coordinate[0]):
-                    legal_move = False
-                    break
+                    # Return if the move is not legal 
+                    return
             updated_grid[coordinate[0], coordinate[1]] = 64
             updated_grid[coordinate[0] + 1, coordinate[1]] = 223
-        if legal_move:
-            self.grid = updated_grid
+        # Update the grid after pacman moved down 
+        self.grid = updated_grid
 
     def is_valid_pip_location(self, x, y):
         if y >= 10 and y <= 15:
@@ -142,35 +136,3 @@ class PacmanEnv(gym.Env):
             return True
         else:
             return False 
-    
-# Create the Pacman environment
-env = PacmanEnv()
-
-# Test environment
-plt.ion() 
-env.render()
-
-#Test moving right
-for i in range(20):
-    env.move_pacman(2)
-
-
-
-#Test moving left
-for i in range(20):
-    env.move_pacman(3)
-
-
-
-# Test moving up
-for i in range(20):
-    env.move_pacman(1)
-
-
-# Test moving down
-for i in range(20):
-    env.move_pacman(4)
-
-
-plt.ioff()
-plt.show()
